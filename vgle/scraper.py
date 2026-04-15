ea0dbda65c91f31b1f7672adb9046ceb52f6f1a6
brinley53 <brinleyhull53@ku.edu>
2026-04-15 16:07:09 -0500

    preprocessing, change inverted index to get from db, posts now go to inverted index

--- vgle/scraper.py
+++ vgle/scraper.py
@@ -2,36 +2,51 @@
 Name: scraper.py
 Description: basic web scraper
 Authors: Brinley Hull & Anakha Krishna
-Other sources: Real Python beautiful soup tutorial (realpython.com)
+Other sources: Real Python beautiful soup tutorial (realpython.com), GeeksForGeeks tutorials
 Created: 3/22/2026
-Last modified: 3/22/2026
+Last modified: 
+    4/13/2026 - add url parsing functionality
 '''
+
+from urllib.parse import urljoin
 
 import requests
 from bs4 import BeautifulSoup
 
-URL = "https://realpython.github.io/fake-jobs/"
-page = requests.get(URL)
+URL = "https://www.geeksforgeeks.org/"
+page = requests.get(URL) # get content of webpage
 
-#print(page.text)
+# check robots.txt 
+# skip all pages if disallowed
+# get all urls from page 
+# add document content to database 
 
+print(page.text)
+
+# get all urls from page
+links = set() 
 soup = BeautifulSoup(page.content, "html.parser")
-results = soup.find(id="ResultsContainer")
+for a in soup.find_all("a", href=True):
+    ref_url = urljoin(URL, a["href"])
+    links.add(ref_url)
 
-python_jobs = results.find_all(
-    "h2", string=lambda text: "python" in text.lower()
-)
+print(links)
+# results = soup.find(id="ResultsContainer")
 
-python_job_cards = [
-    h2_element.parent.parent.parent for h2_element in python_jobs
-]
+# python_jobs = results.find_all(
+#     "h2", string=lambda text: "python" in text.lower()
+# )
 
-for job_card in python_job_cards:
-    title_element = job_card.find("h2", class_="title")
-    company_element = job_card.find("h3", class_="company")
-    location_element = job_card.find("p", class_="location")
-    print(title_element.text.strip())
-    print(company_element.text.strip())
-    print(location_element.text.strip())
-    link_url = job_card.find_all("a")[1]["href"]
-    print(f"Apply here: {link_url}\n")
+# python_job_cards = [
+#     h2_element.parent.parent.parent for h2_element in python_jobs
+# ]
+
+# for job_card in python_job_cards:
+#     title_element = job_card.find("h2", class_="title")
+#     company_element = job_card.find("h3", class_="company")
+#     location_element = job_card.find("p", class_="location")
+#     print(title_element.text.strip())
+#     print(company_element.text.strip())
+#     print(location_element.text.strip())
+#     link_url = job_card.find_all("a")[1]["href"]
+#     print(f"Apply here: {link_url}\n")
